74030e041098228791468fa6a75469a6dc814ffa
Dennismaina <mumbidenis@gmail.com>
2024-09-25 17:15:54 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook,';
+const welcomeMessage = `"WELCOME TO FLATBOOK, " : + ${currentUser}!`;
+//const myString = 'template literal';
+//const myNumber = 10;
+//const myBoolean = false;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `"welcome,": ${currentUser.slice(0,1)}!`;
